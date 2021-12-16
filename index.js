e4536bc43eba996045edb7a61acec1636b52937c
Andres <andrarias@gmail.com>
2021-12-16 11:26:19 -0300

    hasta puppeteer del button

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs/promises'); //para
 
 (async() =>{
     const browser = await puppeteer.launch({
@@ -8,10 +9,17 @@ const puppeteer = require('puppeteer');
     const miUrl ='https://www.youtube.com/'; //youtube
     const miUrl2 = 'https://www.google.com/search?q=ministerio+agricultura+y+pesca&rlz=1C1CHBD_esUY820UY820&oq=ministerio+agr&aqs=chrome.1.0i512l2j69i57j46i512j0i512l6.10108j0j4&sourceid=chrome&ie=UTF-8'; //ministerio Agricultura y pesca
 	const miUrlScreen = 'https://www.cafenix.com.uy/'; // caFenix
+    const addScrap1 = 'https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html'; //pag para escrapear
+    const addScrap2 = 'https://www.instagram.com/elceibo_vivero/?hl=es'; //pag para escrapear
+    const addScrap3 = 'https://books.toscrape.com/catalogue/category/books/food-and-drink_33/index.html'; //pag para escrapear
+    
     const page = await browser.newPage();
     
    //await rascar(miUrl2, page, browser);  //llamada1
-   await screenShoot(miUrlScreen, page, browser); //llamada2
+   //await screenShoot(miUrlScreen, page, browser); //llamada2
+   //await addDatos(addScrap1, page, browser); //llamada3
+   //await getImages(addScrap3, page, browser); //llamada3
+   await click(addScrap3, page, browser); //llamada3
 
 })(); //asi se ejecuta automaticamente al abrir
 
@@ -36,10 +44,65 @@ const puppeteer = require('puppeteer');
 /* screenShoot = async (url, page, browser) => { */
 async function screenShoot(url, page, browser)  {
  await page.goto(url);
- await page.screenshot({path:'miScreen.png'})
+ await page.mouse.click(332,-103,{button:'left'})
+ await page.screenshot({path:'mouse_click.png'});
+ /* await page.screenshot({path:'miScreen.png',fullPage:true}); */
  console.log(`\nFenix-${url}`);
  await browser.close(); //cierra navegador
 }
+
+/* --  guardar datos de alguna pagina   */
+async function addDatos(url, page, browser) {
+    await page.goto(url);
+
+    const listDtosDOM = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll('#default > div > div > div > div > section > div> ol > li > article > h3 > a'))
+                                                    /*
+                                                    #content_inner > article > ul > li> article > h3 > a
+                                                    #content_inner > article > ul > li:nth-child(3) > article > h3 
+                                                    //le saco el nth-child(4) para obtener todos los valores del DOM*/
+          .map(x => x.textContent);
+    });
+    
+
+    const names = ['Ana', 'Daniel', 'Colita', 'Yellow'];
+
+    /*Abajo se crea un nuevo archivo (nuevoArch)y agregara cada linea del 
+    array names en un archivo gracias a fs/promises*/
+    /*//\r\n = return new line*/
+    await fs.writeFile('nuevoArch.txt', listDtosDOM.join('\r\n'));
+
+    console.log(`\r\nFenixche-${listDtosDOM}`);
+
+    await browser.close(); //cierra navegador
+}
+
+/* Guarda ej titulo de las imagenes */
+async function getImages(url, page, browser) {
+    await page.goto(url);
+    const fotos = await page.$$eval('img',imgs=>{
+        return imgs.map(img =>img.src);
+    })
+     for (const foto of fotos) {
+        const imagepage = await page.goto(foto)  /*//navega hasta la url de cada foto
+        /* 
+        #default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(2) > article > div.image_container > a > img
+         #default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(4) > article > div.image_container > a > img */
+        await fs.writeFile(foto.split("/").pop(), await imagepage.buffer());
+            /* se creo un archivo con cada imagen */       
+    }
+    await browser.close(); //cierra navegador
+}
+
+/* click boton */
+async function click(url, page, browser){
+    await page.goto(url);
+    await page.click('#default > div > div > div > div > section > div > ol > li > article > div.product_price > form > button');
+    const datoBoton = await page.$eval('#default > div > div > div > div > section > div > ol > li > article > div.product_price > p.instock.availability',el=>el.textContent)
+    console.log(datoBoton);
+    await browser.close(); //cierra navegador
+}
+
 
 /* **** llmaAsincMio *****  */
 //llmaAsincMio();
